f26e768f8c637bd77017310757dafe87daff59f6
mjh316 <justinhuang316@gmail.com>
2023-05-21 20:05:57 -0700

    I love SVG

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <link rel="shortcut icon" href='/static/bracketcs_logo.svg' />
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/static/bracketcs_logo.svg" />
+      </Head>
+      <div className="flex h-screen flex-col">
+        <Component {...pageProps} />
+        <footer className="flex flex-none justify-center">
+          Copyright &copy; 2023 by BracketCS.
+        </footer>
+      </div>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,65 @@
 import Link from "next/link";
 import Logo from "../../public/static/logo";
-import { motion } from 'framer-motion';
-
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { LeftBrace, RightBrace } from "@/components/Braces";
 
 export default function BracketCSHome() {
   return (
-    <main className="flex flex-col items-center min-h-screen bg-black">
+    <main className="flex flex-1 flex-col items-center bg-black">
       <div className="mt-24 flex flex-col">
         <motion.h1
           initial={{ translateY: -200 }}
           animate={{ translateY: 0 }}
           transition={{ duration: 0.5, type: "spring", stiffness: 100 }}
-          className="lg:text-8xl flex flex-row justify-center md:text-7xl sm:text-6xl text-5xl">
-          &#123;
+          className="flex flex-row items-center justify-center text-5xl sm:text-6xl md:text-7xl lg:text-8xl"
+        >
+          {/* &#123; */}
+          <LeftBrace />
           <motion.div
-            initial={{ opacity: 0, maxWidth: 0, width: 0 }}
-            animate={{ opacity: 1, maxWidth: "max-content", width: "100%" }}
-            transition={{ duration: 3, delay: 0.5, type: "spring", }}
-            className="inline-block overflow-hidden"
+            initial={{ opacity: 0, width: 0 }}
+            animate={{ opacity: 1, width: "100%" }}
+            transition={{
+              duration: 3,
+              delay: 2.75,
+              type: "spring",
+              ease: "easeInOut",
+            }}
+            className="mx-4 inline-block overflow-hidden"
           >
             BracketCS
           </motion.div>
-          &#125;
+          <RightBrace />
+          {/* &#125; */}
         </motion.h1>
       </div>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.5, duration: 0.5, staggerChildren: 0.5 }}
-        className="mt-24 mx-12 flex flex-row gap-x-10">
-        <div className="flex flex-col">
-          <Link className="lg:text-2xl text-lg underline" href="https://www.cerealcodes.org">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 3, duration: 0.5 }}
+        className="mx-12 mt-24 flex flex-row gap-x-8"
+      >
+        <div className="flex flex-col rounded-xl border-2 border-slate-700 p-4 hover:border-slate-400">
+          <Link
+            className="text-sm hover:underline lg:text-2xl"
+            href="https://www.cerealcodes.org"
+          >
             CerealCodes &rarr;
           </Link>
-          <p>Coming in Summer 2023!</p>
+          <p className="mt-2 text-sm">Coming in Summer 2023!</p>
         </div>
-        <div className="flex flex-col">
-          <Link className="lg:text-2xl text-lg underline hover:text-" href="https://www.cerealcodes.org/about">
+        <div className="flex flex-col rounded-xl border-2 border-slate-700 p-4 hover:border-slate-400">
+          <Link
+            className="text-sm hover:underline lg:text-2xl"
+            href="https://www.cerealcodes.org/about"
+          >
             Bracket Team &rarr;
           </Link>
-          <p>The minds behind BracketCS</p>
+          <p className="mt-2 text-sm">The minds behind BracketCS</p>
         </div>
       </motion.div>
+      {/* <LeftBrace /> */}
+      {/* <RightBrace /> */}
     </main>
   );
 }
